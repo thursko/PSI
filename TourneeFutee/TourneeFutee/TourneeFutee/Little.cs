b9643494428ee6a52cd90ee6c79f2529b213012b
thursko <arthur.marion@edu.devinci.fr>
2026-03-20 17:16:15 +0100

    attribut graphe dans little

--- TourneeFutee/TourneeFutee/TourneeFutee/Little.cs
+++ TourneeFutee/TourneeFutee/TourneeFutee/Little.cs
@@ -5,7 +5,7 @@
     public class Little
     {
         // TODO : ajouter tous les attributs que vous jugerez pertinents 
-
+        private Graph graphe;
         // Instancie le planificateur en spécifiant le graphe modélisant un problème de voyageur de commerce
         public Little(Graph graph)
         {
