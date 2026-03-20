4782ff752cff0da0d100b3fcf997c20e3cac486f
Aurélien LESTAEVEL <a.lestaevel@gmail.com>
2026-03-20 19:21:12 +0100

    Tour

--- TourneeFutee/TourneeFutee/TourneeFutee/Tour.cs
+++ TourneeFutee/TourneeFutee/TourneeFutee/Tour.cs
@@ -4,26 +4,36 @@
     public class Tour
     {
         // TODO : ajouter tous les attributs que vous jugerez pertinents 
-
+        
+        private List<(string source, string destination)> segments;
+        private float cost;
         // propriétés
-
+        public Tour()
+        {
+            segments = new List<(string, string)>();
+            cost = 0;
+        }
         // Coût total de la tournée
         public float Cost
         {
-            get;    // TODO : implémenter
+            get { return this.cost; }    // TODO : implémenter
         }
 
         // Nombre de trajets dans la tournée
         public int NbSegments
         {
-            get;    // TODO : implémenter
+            get { return segments.Count; }   // TODO : implémenter
         }
 
-
+        public void AddSegment(string source, string destination, float segmentCost)
+        {
+            segments.Add((source, destination));
+            cost += segmentCost;
+        }
         // Renvoie vrai si la tournée contient le trajet `source`->`destination`
         public bool ContainsSegment((string source, string destination) segment)
         {
-            return false;   // TODO : implémenter 
+            return segments.Contains(segment);   // TODO : implémenter 
         }
 
 
@@ -31,6 +41,13 @@
         public void Print()
         {
             // TODO : implémenter 
+            Console.WriteLine($"Coût total : {cost}");
+            Console.WriteLine("Segments :");
+
+            foreach (var (source, destination) in segments)
+            {
+                Console.WriteLine($"{source} -> {destination}");
+            }
         }
 
         // TODO : ajouter toutes les méthodes que vous jugerez pertinentes 
